ca8dca2324db76e7689e773a84424c20eeb2f1a7
stadnytskyivp <stadnytskyi.volodymyr@gmail.com>
2020-10-22 13:43:29 +0300

    adding frist classes for smartphone page navigator test

--- src/test/java/com/hotline/tests/BaseTest.java
+++ src/test/java/com/hotline/tests/BaseTest.java
@@ -3,9 +3,14 @@ package com.hotline.tests;
 import com.hotline.pageobject.pages.HomePage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Step;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
@@ -17,7 +22,6 @@ public class BaseTest {
     private WebDriver driver;
 
     protected WebDriver getDriver() {
-
         if (driver == null) {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--disable-notifications");
@@ -48,5 +52,25 @@ public class BaseTest {
         if (driver != null) {
             driver.quit();
         }
+    }
+
+    public Boolean isVisibleInViewport(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, 15);
+        driver = ((RemoteWebElement) element).getWrappedDriver();
+        return wait.until((ExpectedCondition<Boolean>) driver -> {
+            assert driver != null;
+            return (Boolean) ((JavascriptExecutor) driver).executeScript(
+                    "var elem = arguments[0],                 " +
+                            "  box = elem.getBoundingClientRect(),    " +
+                            "  cx = box.left + box.width / 2,         " +
+                            "  cy = box.top + box.height / 2,         " +
+                            "  e = document.elementFromPoint(cx, cy); " +
+                            "for (; e; e = e.parentElement) {         " +
+                            "  if (e === elem)                        " +
+                            "    return true;                         " +
+                            "}                                        " +
+                            "return false;                            "
+                    , element);
+        });
     }
 }
